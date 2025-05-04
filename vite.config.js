bd7e8bb5925a2fc3586585b4d245fbbf542eda50
Dave <publiclyviewed@gmail.com>
2025-05-04 11:36:49 +0700

    Added base to config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Recipe-Box-fCC/', // 👈 this line is the key fix
   plugins: [react()],
 })
